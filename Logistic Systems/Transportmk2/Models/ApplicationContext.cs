e746603dbcdf602c3d13234e8fe0d5c84fd9350e
Александр Зубович <minehistory2003@gmail.com>
2023-12-23 09:06:18 +0300

    Added Base code

---
+++ Logistic Systems/Transportmk2/Models/ApplicationContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Transportmk2.Models
+{
+    public class ApplicationContext :DbContext
+    {
+        public DbSet<Storage> Storages { get; set; }
+        public ApplicationContext(DbContextOptions<ApplicationContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();   // создаем базу данных при первом обращении
+        }
+    }
+}
